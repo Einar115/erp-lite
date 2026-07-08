8b987e9c80c7b5771fbc8a72ba149b78d86fdbdc
Einar115 <einarnaim@hotmail.com>
2026-07-08 11:40:22 -0600

    first commit

---
+++ src/test/java/com/einar115/erplite/ErpLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einar115.erplite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
